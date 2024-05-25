d7985f79ade6b2382698f412e8c831d34e52b190
Janak Bahadur Tharu <lightlife908@gmail.com>
2024-05-25 17:10:28 +0545

    cm

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
+    port: parseInt(process.env.PORT) || 5000
   },
   plugins: [react()],
 })
